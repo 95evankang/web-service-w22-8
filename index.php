c1796b0d061ce4685a576625deb6f305305e539a
Evan J. Kang <95evankang@gmail.com>
2022-02-17 09:21:36 -0800

    php

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title> PHP File!</php>
+
+    </head>
+
+
+
+    <body>
+        <h1> PHP File!</h1>
+
+        <?php
+
+        echo "<p> This is PHP </p>";
+
+        ?>
+
+
+
+
+
+
+        <p> THis is HTML</p>
+    </body>
+
+
+
+
+
+
+</html>
